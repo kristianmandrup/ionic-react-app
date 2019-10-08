746271115f76abf3ad88aaba454a69db71fcde70
Kristian Mandrup <kmandrup@gmail.com>
2019-10-08 11:01:12 +0100

    initial app

---
+++ Readme.md
@@ -0,0 +1,13 @@
+# Engine Ionic React foundation app
+
+## Install
+
+`npm i`
+
+## Run in development mode
+
+`npm start`
+
+## Docs
+
+[Ionic React - App overview](https://ionicframework.com/docs/react/your-first-app)
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,23 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonList,
+  IonItem,
+  IonCheckbox,
+  IonLabel,
+  IonNote,
+  IonBadge,
+  IonFab,
+  IonFabButton,
+  IonIcon
+} from "@ionic/react";
+import { add } from "ionicons/icons";
+import React from "react";
 
-const Home: React.FC = () => {
+const Home: React.FC = (props: any) => {
   return (
     <IonPage>
       <IonHeader>
@@ -9,15 +25,24 @@ const Home: React.FC = () => {
           <IonTitle>Ionic Blank</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent className="ion-padding">
-        The world is your oyster.
-        <p>
-          If you get lost, the{' '}
-          <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/">
-            docs
-          </a>{' '}
-          will be your guide.
-        </p>
+      <IonContent>
+        <IonList>
+          <IonItem>
+            <IonCheckbox slot="start" />
+            <IonLabel>
+              <h1>Create Idea</h1>
+              <IonNote>Run Idea by Brandy</IonNote>
+            </IonLabel>
+            <IonBadge color="success" slot="end">
+              5 Days
+            </IonBadge>
+          </IonItem>
+        </IonList>
+        <IonFab vertical="bottom" horizontal="end" slot="fixed">
+          <IonFabButton onClick={() => props.history.push("/new")}>
+            <IonIcon icon={add} />
+          </IonFabButton>
+        </IonFab>
       </IonContent>
     </IonPage>
   );
